1b8272a37b7d193bca120834099b5a8f6af61d24
aboshtop <110741277+aboshtop@users.noreply.github.com>
2022-08-09 01:41:03 +0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-API_TOKEN = os.environ.get('API_TOKEN')
+API_TOKEN = os.environ.get('5567635792:AAELqu_F24oO-3ZQLCghzMRSI2nsrCogzJM')
 SENTRY_DSN = os.getenv('SENTRY_DSN')
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'Local')
 USER_AGENT = os.getenv('USER_AGENT')
